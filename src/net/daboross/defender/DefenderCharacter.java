1b5b0dad3e56afdd41be6fea31d5938a67f7e909
Dabo Ross <dabodialga@gmail.com>
2013-04-04 06:42:17 -0700

    Changes

--- src/net/daboross/defender/DefenderCharacter.java
+++ src/net/daboross/defender/DefenderCharacter.java
@@ -1,10 +1,10 @@
 package net.daboross.defender;
 
 import net.daboross.defender.graphics.FinePoint;
-import net.daboross.defender.graphics.GameGraphics;
 import net.daboross.defender.graphics.GraphicsObject;
 import net.daboross.defender.graphics.HexagonStatics;
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.GL11;
 
 /**
@@ -13,21 +13,23 @@ import org.lwjgl.opengl.GL11;
  */
 public class DefenderCharacter implements GraphicsObject, Updatable {
 
-    private final DefenderMain main;
-    private final GameGraphics gameGraphics;
-    private static final long longestTime = 500;
+    private static final long longestKeyPress = 500;
     private int hexX;
     private int hexY;
-    private long lastUpdate;
     private long lastWPress;
     private long lastSPress;
     private long lastAPress;
+    private long lastDPress;
     private long lastQPress;
     private long lastEPress;
+    private boolean lastWPressed;
+    private boolean lastSPressed;
+    private boolean lastAPressed;
+    private boolean lastDPressed;
+    private boolean lastQPressed;
+    private boolean lastEPressed;
 
-    public DefenderCharacter(DefenderMain main) {
-        this.main = main;
-        this.gameGraphics = main.gameGraphics;
+    public DefenderCharacter() {
     }
 
     public Location getLocation() {
@@ -40,71 +42,122 @@ public class DefenderCharacter implements GraphicsObject, Updatable {
     }
 
     public void update(DefenderMain main) {
-        long time = System.currentTimeMillis();
-        if (time - lastUpdate > longestTime) {
-            lastUpdate = time;
-            if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
-                hexY++;
-            }
-            if (Keyboard.isKeyDown(Keyboard.KEY_S)) {
+        if (isWPressed()) {
+            hexY++;
+        }
+        if (isSPressed()) {
+            hexY--;
+        }
+        if (isAPressed()) {
+            if (hexX % 2 == 0) {
+                hexX--;
+            } else {
+                hexX--;
                 hexY--;
             }
-            if (Keyboard.isKeyDown(Keyboard.KEY_A)) {
-                if (hexX % 2 == 0) {
-                    hexX--;
-                } else {
-                    hexX--;
-                    hexY--;
-                }
-            }
-            if (Keyboard.isKeyDown(Keyboard.KEY_Q)) {
-                if (hexX % 2 == 0) {
-                    hexX--;
-                    hexY++;
-                } else {
-                    hexX--;
-                }
-            }
-            if (Keyboard.isKeyDown(Keyboard.KEY_D)) {
-                if (hexX % 2 == 0) {
-                    hexX++;
-                } else {
-                    hexX++;
-                    hexY--;
-                }
-            }
-            if (Keyboard.isKeyDown(Keyboard.KEY_E)) {
-                if (hexX % 2 == 0) {
-                    hexX++;
-                    hexY++;
-                } else {
-                    hexX++;
-                }
+        }
+        if (isQPressed()) {
+            if (hexX % 2 == 0) {
+                hexX--;
+                hexY++;
+            } else {
+                hexX--;
             }
         }
+        if (isDPressed()) {
+            if (hexX % 2 == 0) {
+                hexX++;
+            } else {
+                hexX++;
+                hexY--;
+            }
+        }
+        if (isEPressed()) {
+            if (hexX % 2 == 0) {
+                hexX++;
+                hexY++;
+            } else {
+                hexX++;
+            }
+        }
+        Keyboard.next();
     }
 
     private boolean isWPressed() {
+        if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
+            if (System.currentTimeMillis() - lastWPress > longestKeyPress || !lastWPressed) {
+                lastWPress = System.currentTimeMillis();
+                lastWPressed = true;
+                return true;
+            } else {
+                lastWPressed = false;
+            }
+        }
         return false;
     }
 
     private boolean isSPressed() {
+        if (Keyboard.isKeyDown(Keyboard.KEY_S)) {
+            if (System.currentTimeMillis() - lastSPress > longestKeyPress || !lastSPressed) {
+                lastSPress = System.currentTimeMillis();
+                lastSPressed = true;
+                return true;
+            } else {
+                lastSPressed = false;
+            }
+        }
         return false;
     }
 
     private boolean isAPressed() {
+        if (Keyboard.isKeyDown(Keyboard.KEY_A)) {
+            if (System.currentTimeMillis() - lastAPress > longestKeyPress || !lastAPressed) {
+                lastAPress = System.currentTimeMillis();
+                lastAPressed = true;
+                return true;
+            } else {
+                lastAPressed = false;
+            }
+        }
         return false;
     }
 
     private boolean isDPressed() {
+        if (Keyboard.isKeyDown(Keyboard.KEY_D)) {
+            if (System.currentTimeMillis() - lastDPress > longestKeyPress || !lastDPressed) {
+                lastDPress = System.currentTimeMillis();
+                lastDPressed = true;
+                return true;
+            } else {
+                lastDPressed = false;
+            }
+        }
         return false;
     }
 
     private boolean isQPressed() {
+        if (Keyboard.isKeyDown(Keyboard.KEY_Q)) {
+            if (System.currentTimeMillis() - lastQPress > longestKeyPress || !lastQPressed) {
+                lastQPress = System.currentTimeMillis();
+                lastQPressed = true;
+                return true;
+            } else {
+                lastQPressed = false;
+            }
+        }
         return false;
     }
 
     private boolean isEPressed() {
+        if (Keyboard.isKeyDown(Keyboard.KEY_E)) {
+            if (System.currentTimeMillis() - lastEPress > longestKeyPress || !lastEPressed) {
+                lastEPress = System.currentTimeMillis();
+                lastEPressed = true;
+                return true;
+            } else {
+                lastEPressed = false;
+            }
+        }
         return false;
     }
 }
